8bfbc289d8421bfc15b8035333bbdde16234cc99
Marc <m.bonte@rubika-edu.com>
2019-10-01 16:09:40 +0200

    Basic commit
    - Added a generic sound
    - Added generic materials
    - Added a very basic debugger in scene (timescale inc, or dec, or back
    to 1, reload scene)
    - No more auto light building
    - Ultra basic prefabs (main camera, main light, debugger)

---
+++ Assets/Scripts/Debug/Debugger.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Debugger : MonoBehaviour
+{
+    [Header("Time Management")]
+
+    [Range(0f, 1f)]
+    public float slowSpeed = 0.5f;
+    [Range(1f, 10f)]
+    public float fastSpeed = 2f;
+
+    private void Update()
+    {
+        // SCENE MANAGEMENT
+        // RELOAD
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        // TIME MANAGEMENT
+        // FAST
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            Time.timeScale = fastSpeed;
+        }
+        // SLOW
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            Time.timeScale = slowSpeed;
+        }
+        // BACK TO NORMAL
+        if (Input.GetKeyDown(KeyCode.KeypadMultiply))
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
